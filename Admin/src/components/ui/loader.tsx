c49937c78e768cb67c27e95e813dbf43b0806e1e
Devdanny <Devdanny2024@gmail.com>
2025-11-25 18:01:22 +0100

    Cleaned repo and added gitignore

--- Admin/src/components/ui/loader.tsx
+++ Admin/src/components/ui/loader.tsx
@@ -7,3 +7,4 @@ export function Loader({ size = 16 }: { size?: number }) {
     />
   );
 }
+
--- Admin/src/components/ui/toaster.tsx
+++ Admin/src/components/ui/toaster.tsx
@@ -3,3 +3,4 @@ import { Toaster as SonnerToaster } from "sonner";
 export function Toaster(props: React.ComponentProps<typeof SonnerToaster>) {
   return <SonnerToaster theme="dark" {...props} />;
 }
+
