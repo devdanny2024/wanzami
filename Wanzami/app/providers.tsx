e7d9b61c9d508a921f79580b6379bf2b5b0c4ce8
Devdanny v2 <solvvitalik@gmail.com>
2025-12-22 22:53:08 +0100

    Add global top loader for navigation

--- Wanzami/app/providers.tsx
+++ Wanzami/app/providers.tsx
@@ -2,10 +2,19 @@
 
 import { CookieConsent } from "@/components/CookieConsent";
 import { Toaster } from "@/components/ui/toaster";
+import NextTopLoader from "nextjs-toploader";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <NextTopLoader
+        color="#fd7e14"
+        height={3}
+        showSpinner={false}
+        crawl
+        easing="ease"
+        speed={400}
+      />
       {children}
       <CookieConsent />
       <Toaster richColors position="top-right" />
