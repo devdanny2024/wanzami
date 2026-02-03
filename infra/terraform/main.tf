ce2c5946625701d2e021177cc015ec3795a9f45c
Devdanny v2 <solvvitalik@gmail.com>
2026-02-03 19:52:45 +0100

    Attach new target group to existing ALB via listener rule

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -272,6 +272,29 @@ resource "aws_lb_listener" "http" {
   }
 }
 
+data "aws_lb_listener" "http_existing" {
+  count             = var.use_existing_alb ? 1 : 0
+  load_balancer_arn = local.alb_arn
+  port              = 80
+}
+
+resource "aws_lb_listener_rule" "api_forward" {
+  count        = var.use_existing_alb ? 1 : 0
+  listener_arn = data.aws_lb_listener.http_existing[0].arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = local.tg_arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/api/*"]
+    }
+  }
+}
+
 # HTTPS listener + ACM cert
 resource "aws_acm_certificate" "api" {
   count             = var.api_domain != "" ? 1 : 0
