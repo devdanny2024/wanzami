f7bb1afa0a9c251fbbbcef62c4d54400803b99e1
Devdanny v2 <solvvitalik@gmail.com>
2026-02-03 18:37:03 +0100

    Set ALB target group type to ip for Fargate

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -190,6 +190,7 @@ resource "aws_lb_target_group" "backend" {
   port     = 4000
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  target_type = "ip"
 
   health_check {
     path                = "/api/health"
